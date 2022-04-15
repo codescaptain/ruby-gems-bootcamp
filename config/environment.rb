bc4510b574ed2648fb004a9718fe4ef4f1fff826
codescaptain <ahmet-57-@hotmail.com>
2022-04-16 02:47:54 +0300

    [add] fields to Course model

--- config/environment.rb
+++ config/environment.rb
@@ -5,8 +5,6 @@ require_relative "application"
 # Initialize the Rails application.
 Rails.application.initialize!
 
-# Initialize the Rails application.
-Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
   :address => 'smtp.sendgrid.net',
