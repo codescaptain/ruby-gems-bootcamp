3b38c24d3a5a1ddab405d8c0a15a606ade4908fa
codescaptain <ahmet-57-@hotmail.com>
2022-04-16 01:12:45 +0300

    [add] add devise trackable

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :courses
   root 'static_pages#landing_page'
   get 'privacy_policy', to: 'static_pages#privacy_policy'
+  resources :users, only: [:index]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
