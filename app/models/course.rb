b56628c45aa15bfba27068983f8ba272653dc67c
codescaptain <ahmet-57-@hotmail.com>
2022-04-18 01:26:38 +0300

    [add] ransack gem

--- app/models/course.rb
+++ app/models/course.rb
@@ -11,12 +11,12 @@ class Course < ApplicationRecord
   has_rich_text :description
 
   extend FriendlyId
-  #friendly_id :title, use: :slugged
+  friendly_id :title, use: :slugged
   #
-  friendly_id :generated_slug, use: :slugged
-  def generated_slug
-    require 'securerandom'
-    @random_slug ||= persisted? ? friendly_id : SecureRandom.hex(4)
-  end
+  # friendly_id :generated_slug, use: :slugged
+  # def generated_slug
+  #   require 'securerandom'
+  #   @random_slug ||= persisted? ? friendly_id : SecureRandom.hex(4)
+  # end
 end
 
