7557a6688728cfc978828acca2d5e4bd0b8a8da3
codescaptain <ahmet-57-@hotmail.com>
2022-04-19 01:17:35 +0300

    [add] #show #edit method in users_controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root 'static_pages#landing_pages'
   get 'static_pages/activity'
   get 'privacy_policy', to: 'static_pages#privacy_policy'
-  resources :users, only: [:index]
+  resources :users, only: [:index, :edit, :update, :show]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
