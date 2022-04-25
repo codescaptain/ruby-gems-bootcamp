8a4cd1b8f66771030f2921c163928f570da41462
codescaptain <ahmet-57-@hotmail.com>
2022-04-26 01:59:53 +0300

    [update] lesson and course views

--- app/policies/lesson_policy.rb
+++ app/policies/lesson_policy.rb
@@ -22,7 +22,7 @@ class LessonPolicy < ApplicationPolicy
   end
 
   def create?
-    #@user.has_role?(:teacher)
+    @user.has_role?(:admin) || @record.course.user_id == @user.id
   end
 
   def destroy?
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :lessons
+
   devise_for :users
-  resources :courses
+  resources :courses do
+    resources :lessons
+  end
   root 'static_pages#landing_pages'
   get 'static_pages/activity'
   get 'privacy_policy', to: 'static_pages#privacy_policy'
