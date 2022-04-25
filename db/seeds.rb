91356f45bb4eeb72e652543068cbda0a9e9248b6
codescaptain <ahmet-57-@hotmail.com>
2022-04-26 02:49:03 +0300

    [update] design for courses and lessons

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,13 @@
-User.create!(
-  email:'admin@example.com',
-  password:'123456',
-  password_confirmation:'123456',
-)
+#User.create!(email: 'admin@example.com', password: 'admin@example.com', password_confirmation: 'admin@example.com')
+# user = User.new(
+#   email: 'admin@example.com',
+#   password: 'admin@example.com',
+#   password_confirmation: 'admin@example.com'
+# )
+# user.skip_confirmation!
+# user.save!
 
+PublicActivity.enabled = false
 30.times do
   Course.create!([{
                     title: Faker::Educator.course_name,
@@ -15,3 +19,4 @@ User.create!(
                     price: Faker::Number.between(from: 1000, to: 20000)
                   }])
 end
+PublicActivity.enabled = true
